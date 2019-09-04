8b7e8bde03ca269d0650ff0309ed4e0caa1c8f91
meganecummings <megcummings@gmail.com>
2019-09-04 11:40:49 -0700

    Event Routes Update

--- routes/events.js
+++ routes/events.js
@@ -8,8 +8,8 @@ const authRequired = require('../middleware/authRequired');
 // Event Register Routes
 router.get('/', ctrl.event.index);
 router.get('/:event_id', ctrl.event.show);
-router.put('/:event_id', ctrl.event.update);
-router.delete('/:event_id', ctrl.event.delete);
+router.put('/:event_id/update', ctrl.event.update);
+router.delete('/:event_id/delete', ctrl.event.delete);
 router.post('/new', ctl.event.create);
 
 module.exports = router;
