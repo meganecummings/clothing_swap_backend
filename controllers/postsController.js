62a8619d30455327fa909f95a6457c9f21a533b5
meganecummings <megcummings@gmail.com>
2019-09-04 15:14:02 -0700

    postController Init

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -35,7 +35,6 @@ module.exports = {
         });
     },
     create: (request, response) => {
-        const errors = [];
         const newPost = request.body;
 
         db.Post.create(newPost, (error, createdPost) => {
@@ -51,7 +50,51 @@ module.exports = {
             });
 
             // Add Post to User/Author
-            db.User.findOneAndUpdate({  })
-        })
+            db.User.findOneAndUpdate({ username: newPost.username }, { new: true }, (error, foundUser) => {
+                if (error) return foundUser.posts.push(createdPost);
+                foundUser.save()
+            })
+                .populate('posts')
+                .exec((error, user) => {
+                    if (error) return console.log(error);
+                    console.log(user)
+                })
+            // Add Post to Relevant Event 
+            db.Event.findOneAndUpdate({ slug: newPost.event_slug }, { new: true }, (error, foundEvent) => {
+                if (error) return console.log(error);
+                foundEvent.posts.push(createdPost);
+                foundEvent.save()
+            })
+            .populate('posts')
+            .exec((error, event) => {
+                if (error) return console.log(error)
+                console.log(event)
+            })
+        });
+    },
+    edit: (request, response) => {
+        db.Post.findByIdAndUpdate(request.params.post_id, request.body, { new: true }, (error, updatedPost) => {
+            if (error) return request.status(400).json({
+                status: 400,
+                message: 'Something went wrong, please try again'
+            });
+            response.status(202).json({
+                status: 202, 
+                data: updatedPost, 
+                requestedAt: getTime()
+            });
+        });
+    },
+    delete: (request, response) => {
+        db.Post.findByIdAndDelete(request.params.post_id, (error, deletedPost) => {
+            if (error) return response.status(400).json({
+                status: 400,
+                message: 'Something went wrong. Please try again.'
+            });
+            response.status(200).json({
+                status: 200, 
+                message: `Success! You've deleted ${deletedPost.title}`
+            });
+        });
     }
-}
+};
