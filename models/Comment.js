1e4ebb8569d05653eb7412c2078a6797515266c9
meganecummings <megcummings@gmail.com>
2019-09-04 11:25:08 -0700

    Updated Comment Model Again

--- models/Comment.js
+++ models/Comment.js
@@ -2,23 +2,23 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
-    title: {
-        type: String,
-        required: true,
-        maxlength: 200,
-        minlength: 3
-    },
     content: {
         type: String,
         required: true,
         maxlength: 500
     },
-    user: [
+    user_id: [
         {
             type: Schema.Types.ObjectId,
             ref: 'User',
         }
     ],
+    post_id: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Post',
+        }
+    ], 
     date_commented: {
         type: Date,
         default: Date.now
--- routes/posts.js
+++ routes/posts.js
@@ -3,4 +3,13 @@ const router = express.Router();
 const ctrl = require('../controllers');
 const authRequired = require('../middleware/authRequired');
 
-// Endpoint ==> 'api/v1
+// Endpoint ==> 'api/v1/posts'
+
+// Posts Route
+router.get('/:post_id', ctrl.post.show);
+router.get('/', ctrl.post.index);
+router.put('/:post_id/edit', ctrl.post.edit);
+router.put('/new',ctrl.post.create);
+router.delete('/:post_id', ctrl.post.delete);
+
+module.exports = router;
