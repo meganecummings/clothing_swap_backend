ab7cadd8982e8aacc4651ce1da6939544de27ec1
meganecummings <megcummings@gmail.com>
2019-09-20 13:26:13 -0700

    Updated CORS options & SendGrid .env

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(session({
 }));
 
 const corsOptions = {
-    origin: ['http://localhost:3000'],
+    origin: ['https://meganecummings.github.io/clothingswap-app/'],
     credentials: true,
     optionsSuccessStatus: 200
 }
