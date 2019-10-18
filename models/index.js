50e5460a403051ff843750bf0a0ce7fb9f323cd1
meganecummings <megcummings@gmail.com>
2019-10-17 20:21:15 -0700

    Using MONGODB_URI

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const DB_URL = process.env.DB_URI || 'mongodb://localhost:27017/clothingswap';
+const DB_URL = process.env.MONGODB_URI || 'mongodb://localhost:27017/clothingswap';
 
 console.log(DB_URL);
 
